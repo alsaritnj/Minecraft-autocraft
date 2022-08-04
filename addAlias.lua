138f0e53c7b122a48f5af4d70de0fbe2a2ea3d57
alsaritnj <alsaritnj@gmail.com>
2022-08-04 22:45:08 +0300

    addAlias() remaked

--- addAlias.lua
+++ addAlias.lua
@@ -1,11 +1,5 @@
+require("addTextToEndOfTableInFile")
+
 function addAlias(name, alias, fileName)
-    local aliases = io.open(fileName, "r")
-    local content = aliases:read("a")
-    io.close(aliases)
-
-    content =  string.gsub(content, "\n}", "") .. ",\n\t\"" .. name .."\" = \"" .. alias .. "\"\n}"
-
-    aliases = io.open(fileName, "w")
-    aliases:write(content)
-    io.close(aliases)
+    addTextToEndOfTableInFile(("\"" .. alias .."\" = \"" .. name .. "\""), itemName)
 end
