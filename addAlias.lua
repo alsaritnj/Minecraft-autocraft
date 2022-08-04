896c656fc7880055734460ffd895e8ff96375147
alsaritnj <alsaritnj@gmail.com>
2022-08-04 21:57:50 +0300

    addAlias fixed

--- addAlias.lua
+++ addAlias.lua
@@ -1,11 +1,11 @@
-function addAlias(name, alias)
-    local aliases = io.open("aliases.lua", "r")
+function addAlias(name, alias, fileName)
+    local aliases = io.open(fileName, "r")
     local content = aliases:read("a")
     io.close(aliases)
 
     content =  string.gsub(content, "\n}", "") .. ",\n\t\"" .. name .."\" = \"" .. alias .. "\"\n}"
 
-    aliases = io.open("aliases.lua", "w")
+    aliases = io.open(fileName, "w")
     aliases:write(content)
     io.close(aliases)
 end
