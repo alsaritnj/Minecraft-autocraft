c97b9338d2250dd0c2c68f82c9bd3d07fc06a239
alsaritnj <alsaritnj@gmail.com>
2022-08-10 12:44:36 +0300

    Recipes and aliases appdated

---
+++ craftStationsAliases.lua
@@ -0,0 +1,13 @@
+return
+{
+	CraftingTable = "Crafting Table",
+	RollingMachine = "Forge Hammer",
+	CutingMachine = "Cutter",
+	WireMachine = "Copper Cable",
+	electric_furnace = "Electric Furnace",
+	ElectricFurnace = "Electric Furnace",
+	compressor = "Compressor",
+	Compressor = "Compressor",
+	macerator = "Macerator",
+	Macerator = "Macerator"
+}
--- recipes.lua
+++ recipes.lua
@@ -1,88 +1,281 @@
 -- the separetad table for each type made for easy data enter
 local workbenchRecipes = 
 {
-    {itemName = "copper insulated wire",
-        receivedCount = 1, craftStationName = "workbench",
-        recipe = 
-        {
-            {itemName = "copper wire", needCount = 1}, {itemName = "rudder", needCount = 1}
-        },
-        materials = 
-        {
-            {itemName = "copper wire", needCount = 1}, {itemName = "rudder", needCount = 1}
-        }
-    },
-
-	{itemName = "part circuit", 
-        receivedCount = 1, craftStationName = "workbench",
-        recipe = 
-        {
-            {itemName = "copper insulated wire", needCount = 1}, {itemName = "copper insulated wire", needCount = 1}, {itemName = "copper insulated wire", needCount = 1},
-            {itemName = "redstone", needCount = 1}, {itemName = "iron plate", needCount = 1}, {itemName = "redstone", needCount = 1},
-            {itemName = "copper insulated wire", needCount = 1}, {itemName = "copper insulated wire", needCount = 1}, {itemName = "copper insulated wire", needCount = 1}
-        },
-        materials = 
-        {
-            {itemName = "copper insulated wire", needCount = 6}, {itemName = "redstone", needCount = 2}, {itemName = "iron plate", needCount = 1}
-        }
-    },
-
-	{itemName = "stick", 
-        receivedCount = 4, craftStationName = "workbench", 
-        recipe = 
-        {
-            {itemName = "wood", needCount = 1}, nil, nil, {itemName = "wood", needCount = 1}
-        }, 
-        materials = 
-        {
-            {itemName = "wood", needCount = 2}
-        }
-    }
+    {itemName = "Heat Vent",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Iron Plate", needCount = 1}, {itemName = "Iron Bars", needCount = 1}, 
+			{itemName = "Iron Plate", needCount = 1}, {itemName = "Electric Motor", needCount = 1}, {itemName = "Iron Plate", needCount = 1}, 
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Iron Plate", needCount = 1}, {itemName = "Iron Bars", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Iron Bars", needCount = 4}, {itemName = "Iron Plate", needCount = 4}, {itemName = "Electric Motor", needCount = 1}
+		}
+	},
+	{itemName = "Reactor Heat Vent",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Copper Plate", needCount = 1}, {itemName = "Copper Plate", needCount = 1}, {itemName = "Copper Plate", needCount = 1}, 
+			{itemName = "Copper Plate", needCount = 1}, {itemName = "Heat Vent", needCount = 1}, {itemName = "Copper Plate", needCount = 1}, 
+			{itemName = "Copper Plate", needCount = 1}, {itemName = "Copper Plate", needCount = 1}, {itemName = "Copper Plate", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Copper Plate", needCount = 8}, {itemName = "Heat Vent", needCount = 1}
+		}
+	},
+	{itemName = "Overclocked Heat Vent",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			nil, {itemName = "Gold Plate", needCount = 1}, nil, 
+			{itemName = "Gold Plate", needCount = 1}, {itemName = "Reactor Heat Vent", needCount = 1}, {itemName = "Gold Plate", needCount = 1}, 
+			nil, {itemName = "Gold Plate", needCount = 1}, nil
+		},
+		materials =
+		{
+			{itemName = "Gold Plate", needCount = 4}, {itemName = "Reactor Heat Vent", needCount = 1}
+		}
+	},
+	{itemName = "Component Heat Vent",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Tin Plate", needCount = 1}, {itemName = "Iron Bars", needCount = 1}, 
+			{itemName = "Tin Plate", needCount = 1}, {itemName = "Heat Vent", needCount = 1}, {itemName = "Tin Plate", needCount = 1}, 
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Tin Plate", needCount = 1}, {itemName = "Iron Bars", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Iron Bars", needCount = 4}, {itemName = "Tin Plate", needCount = 4}, {itemName = "Heat Vent", needCount = 1}
+		}
+	},
+	{itemName = "Advanced Heat Vent",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Heat Vent", needCount = 1}, {itemName = "Iron Bars", needCount = 1}, 
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Diamond", needCount = 1}, {itemName = "Iron Bars", needCount = 1}, 
+			{itemName = "Iron Bars", needCount = 1}, {itemName = "Heat Vent", needCount = 1}, {itemName = "Iron Bars", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Iron Bars", needCount = 6}, {itemName = "Heat Vent", needCount = 2}, {itemName = "Diamond", needCount = 1}
+		}
+	},
+	{itemName = "Insulated Copper Cable",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Copper Cable", needCount = 1}, {itemName = "Rubber", needCount = 1}, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		
+		materials =
+		{
+			{itemName = "Copper Cable", needCount = 1}, {itemName = "Rubber", needCount = 1}
+		}
+	},
+	{itemName = "Insulated Tin Cable",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Tin Cable", needCount = 1}, {itemName = "Rubber", needCount = 1}, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Tin Cable", needCount = 1}, {itemName = "Rubber", needCount = 1}
+		}
+	},
+	{itemName = "Electric Motor",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			nil, {itemName = "Tin Item Casing", needCount = 1}, nil, 
+			{itemName = "Coil", needCount = 1}, {itemName = "Iron Ingot", needCount = 1}, {itemName = "Coil", needCount = 1}, 
+			nil, {itemName = "Tin Item Casing", needCount = 1}, nil
+		},
+		materials =
+		{
+			{itemName = "Tin Item Casing", needCount = 2}, {itemName = "Coil", needCount = 2}, {itemName = "Iron Ingot", needCount = 1}
+		}
+	},
+	{itemName = "Electronic Circuit",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Insulated Copper Cable", needCount = 1}, {itemName = "Insulated Copper Cable", needCount = 1}, {itemName = "Insulated Copper Cable", needCount = 1}, 
+			{itemName = "Redstone", needCount = 1}, {itemName = "Iron Plate", needCount = 1}, {itemName = "Redstone", needCount = 1}, 
+			{itemName = "Insulated Copper Cable", needCount = 1}, {itemName = "Insulated Copper Cable", needCount = 1}, {itemName = "Insulated Copper Cable", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Insulated Copper Cable", needCount = 6}, {itemName = "Redstone", needCount = 2}, {itemName = "Iron Plate", needCount = 1}
+		}
+	},
+	{itemName = "Transistor",
+		receivedCount = 8, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Iron Ingot", needCount = 1}, {itemName = "Iron Ingot", needCount = 1}, {itemName = "Iron Ingot", needCount = 1}, 
+			{itemName = "Gold Nugget", needCount = 1}, {itemName = "Paper", needCount = 1}, {itemName = "Gold Nugget", needCount = 1}, 
+			nil, {itemName = "Redstone", needCount = 1}, nil
+		},
+		materials =
+		{
+			{itemName = "Iron Ingot", needCount = 3}, {itemName = "Gold Nugget", needCount = 2}, {itemName = "Paper", needCount = 1}, 
+			{itemName = "Redstone", needCount = 1}
+		}
+	},
+	{itemName = "Coil",
+		receivedCount = 1, craftStationName = "CraftingTable",
+		recipe =
+		{
+			{itemName = "Copper Cable", needCount = 1}, {itemName = "Copper Cable", needCount = 1}, {itemName = "Copper Cable", needCount = 1}, 
+			{itemName = "Copper Cable", needCount = 1}, {itemName = "Iron Ingot", needCount = 1}, {itemName = "Copper Cable", needCount = 1}, 
+			{itemName = "Copper Cable", needCount = 1}, {itemName = "Copper Cable", needCount = 1}, {itemName = "Copper Cable", needCount = 1}
+		},
+		materials =
+		{
+			{itemName = "Copper Cable", needCount = 8}, {itemName = "Iron Ingot", needCount = 1}
+		}
+	},
 }
 
 local wireMachineRecipes =
 {
-    {itemName = "copper wire",
-        receivedCount = 3, craftStationName = "wire machine",
-        recipe = 
-        {
-            {itemName = "copper ingot", needCount = 1}
-        },
-        materials = 
-        {
-            {itemName = "copper ingot", needCount = 1}
-        }
-    }
+    {itemName = "Copper Cable",
+		receivedCount = 3, craftStationName = "WireMachine",
+		recipe =
+		{
+			{itemName = "Copper Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Copper Ingot", needCount = 1}
+		}
+	},
+	{itemName = "Tin Cable",
+		receivedCount = 3, craftStationName = "WireMachine",
+		recipe =
+		{
+			{itemName = "Tin Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Tin Ingot", needCount = 1}
+		}
+	},
 }
 
 local rollingMachineRecipes = 
 {
-    {itemName = "iron plate",
-        receivedCount = 1, craftStationName = "rolling machine", 
-        recipe = 
-        {
-            {itemName = "iron ingot", needCount = 1}
-        }, 
-        materials = 
-        {
-            {itemName = "iron ingot", needCount = 1}
-        }
-    }
+   	{itemName = "Iron Item Casing",
+		receivedCount = 2, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Iron Plate", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Iron Plate", needCount = 1}
+		}
+	},
+	{itemName = "Copper Item Casing",
+		receivedCount = 2, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Copper Plate", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Copper Plate", needCount = 1}
+		}
+	},
+	{itemName = "Tin Item Casing",
+		receivedCount = 2, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Tin Plate", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Tin Plate", needCount = 1}
+		}
+	},
+	{itemName = "Copper Plate",
+		receivedCount = 1, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Copper Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Copper Ingot", needCount = 1}
+		}
+	},
+	{itemName = "Tin Plate",
+		receivedCount = 1, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Tin Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Tin Ingot", needCount = 1}
+		}
+	},
+	{itemName = "Iron Plate",
+		receivedCount = 1, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Iron Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Iron Ingot", needCount = 1}
+		}
+	},
+	{itemName = "Gold Plate",
+		receivedCount = 1, craftStationName = "RollingMachine",
+		recipe =
+		{
+			{itemName = "Gold Ingot", needCount = 1}, nil, nil, 
+			nil, nil, nil, 
+			nil, nil, nil
+		},
+		materials =
+		{
+			{itemName = "Gold Ingot", needCount = 1}
+		}
+	}
 }
 
 local compressorRecipes = 
 {
-    {itemName = "energy crystal",
-        receivedCount = 1, craftStationName = "compressor", 
-        recipe = 
-        {
-            {itemName = "energy dust", needCount = 9}
-        }, 
-        materials = 
-        {
-            {itemName = "energy dust", needCount = 9}
-        }
-    }
+    
 }
 
 -- we are merging all tables with recipes in one
